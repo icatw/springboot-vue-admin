bc16846e8e473b84d748165ccd54513ee90db2a7
icatw <762188827@qq.com>
2022-04-26 02:08:21 +0800

    Initial commit

---
+++ springboot/src/test/java/cn/icatw/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.icatw.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
